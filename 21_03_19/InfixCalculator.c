47b95cd8c936526f3c7df02ccb6800cafc8a112b
user name <79510152+duthd3@users.noreply.github.com>
2021-03-19 15:42:56 +0900

    Create InfixCalculator.c

---
+++ 21_03_19/InfixCalculator.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdlib.h>
+#include "InfixToPostfix.h"
+#include "PostCalculator.h"
+
+int EvalInfixExp(char exp[])
+{
+	int len = strlen(exp);
+	int ret;
+	char * expcpy = (char*)malloc(len+1);
+	strcpy(expcpy, exp);
+
+	ConvToRPNExp(expcpy);    
+	ret = EvalRPNExp(expcpy);
+
+	free(expcpy);
+	return ret;
+}
