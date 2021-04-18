51fac9d76f29ac6eca75671b7399dbde7f4a3d1b
YeoSong <duthd3@naver.com>
2021-04-18 23:52:27 +0900

    Create DBLinkedListMain.c

---
+++ LinkedList/DBLinkedList/DBLinkedListMain.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "DBLinkedList.h"
+
+int main(void)
+{
+	// 양방향 연결 리스트의 생성 및 초기화  ///////
+	List list;
+	int data;
+	ListInit(&list);
+
+	// 8개의 데이터 저장  ///////
+	LInsert(&list, 2);  LInsert(&list, 4);
+	LInsert(&list, 8);  
+	
+
+	// 저장된 데이터의 조회  ///////
+	if(LFirst(&list, &data))
+	{
+		printf("%d ", data);
+
+		while(LNext(&list, &data)) 
+			printf("%d ", data);
+		
+		while(LPrevious(&list, &data))
+			printf("%d ", data);
+		
+		printf("\n\n");
+	}
+
+	return 0;
+}
